e6549d745b985ce04f463b33c7a25ae99cda72e0
Christian Black <blackl3@gmail.com>
2023-03-04 10:58:33 -0600

    user can now reset chores on to do page

--- pages/todo/index.js
+++ pages/todo/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { resetToDo } from '../../api/choreData';
 import { getSingleHousehold } from '../../api/householdData';
 import ProfileChoreCard from '../../components/ProfileChoreCard';
 import { useAuth } from '../../utils/context/authContext';
@@ -17,6 +18,10 @@ export default function TodoPage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user.household]);
 
+  const resetChoresToDoList = () => {
+    resetToDo(householdDetails.chores[0].id, user.uid).then(() => getPageContent());
+  };
+
   return (
     <>
       {user.household?.id ? (
@@ -25,8 +30,13 @@ export default function TodoPage() {
           <Button className="household-buttons" href={`/household/edit/${user.household?.id}`}>
             Manage Household
           </Button>
+          <Button className="houshold-buttons" onClick={resetChoresToDoList} variant="outline-success">
+            Reset List
+          </Button>
         </>
-      ) : <h1>Please Add Yourself to a Household, or Create a new Household</h1>}
+      ) : (
+        <h1>Please Add Yourself to a Household, or Create a new Household</h1>
+      )}
       {householdDetails.chores?.map((chore) => (
         <ProfileChoreCard photoUrl={chore.owner ? chore.owner.photo_url : null} key={chore.id} obj={chore} onUpdate={getPageContent} />
       ))}
